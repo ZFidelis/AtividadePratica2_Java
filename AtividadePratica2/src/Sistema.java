dfeb3250b5f7be8ac5d518e1837ad63bed63b12a
ZFidelis <isaaczapo@hotmail.com>
2024-04-16 21:10:51 -0300

    Sistema class creation

---
+++ AtividadePratica2/src/Sistema.java
@@ -0,0 +1,33 @@
+public class Sistema {
+    public static void menuPrincipal(){
+
+        System.out.println("-- Menu Principal --");
+
+        System.out.println("1)Cadastros");
+        System.out.println("2)Lista");
+        System.out.println("3)Exclusões");
+        System.out.println("4)Encerrar Programa");
+
+    }
+
+    public static void menuCadastros(){
+
+        System.out.println("-- Menu de Cadastros --");
+
+        System.out.println("1)Cadastrar ");
+        System.out.println("2)Cadastrar ");
+        System.out.println("3)Cadastrar ");
+    }
+
+    public static void menuListas(){
+
+        System.out.println("-- Menu de Listagem --");
+
+        System.out.println("1)Listar ");
+        System.out.println("2)Listar ");
+        System.out.println("3)Listar ");
+        System.out.println("4)Listar Todos");
+    }
+
+    public static void menuExclusões(){}
+}
